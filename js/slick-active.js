6cf4271f815bccf6d1b9d192252a3e3742ceed36
BStojek <es.bartek@gmail.com>
2020-02-02 21:21:14 +0100

    convert img

--- js/slick-active.js
+++ js/slick-active.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
         responsive: [{
             breakpoint: 1024,
             settings: {
-
+                draggable: false,
             }
         }],
     });
