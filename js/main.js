d5251cc56e155489ec5564be904ff286779d95cc
BStojek <es.bartek@gmail.com>
2020-02-23 23:16:17 +0100

    finish about with scripts

--- js/main.js
+++ js/main.js
@@ -26,12 +26,36 @@ const navSlide = () => {
 navSlide();
 
 $(document).ready(function () {
+
     $(".banner__btn").click(function () {
-        var pos = $(".about").position();
+        let pos = $(".about").position();
 
         $('body,html').animate({
             scrollTop: pos.top,
         }, 1000);
 
     });
+
+    $(window).on('scroll', function () {
+        let pos = $(window).scrollTop() + 250;
+        let pos1 = $('.about-info--first').offset().top + $('.about-info--first').outerHeight() - window.innerHeight;
+        let pos2 = $('.about-info--second').offset().top + $('.about-info--first').outerHeight() - window.innerHeight;
+        console.log(pos);
+        console.log(pos1);
+        if (pos >= pos1) {
+            console.log($(window).scrollTop())
+            console.log('You reached the end of the DIV');
+            $(".about-info__img-wrap--first").addClass('about-active');
+            $(".about-info__content--first").addClass('about-active');
+        }
+        if (pos >= pos2) {
+            console.log($(window).scrollTop())
+            console.log('You reached the end of the DIV');
+            $(".about-info__img-wrap--second").addClass('about-active');
+            $(".about-info__content--second").addClass('about-active');
+
+        }
+
+    });
+
 });
