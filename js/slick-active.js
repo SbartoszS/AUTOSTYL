ba7c16226c392a3638ca5638df158bd4cb696b49
BStojek <es.bartek@gmail.com>
2020-04-07 15:00:49 +0200

    end carousel logo, and correct about section

--- js/slick-active.js
+++ js/slick-active.js
@@ -19,3 +19,36 @@ $(document).ready(function () {
         }],
     });
 });
+
+$(document).ready(function () {
+    $('.adv-wrapper').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: false,
+        dots: false,
+        pauseOnHover: true,
+        draggable: false,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+
+                }
+            }, {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 520,
+                settings: {
+                    slidesToShow: 2
+                }
+            }
+        ]
+    });
+});
